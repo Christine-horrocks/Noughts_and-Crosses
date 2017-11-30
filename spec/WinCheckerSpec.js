0551d4c76f50ed712dedbab1346f6e5a2dddf9ec
Christine <splogy2001@hotmail.com>
2017-11-30 16:21:28 +0000

    columnChecker tested and working

--- spec/WinCheckerSpec.js
+++ spec/WinCheckerSpec.js
@@ -10,14 +10,28 @@ describe("winChecker", function() {
     expect(winChecker.rowChecker(gArr)).toEqual("Congratulations! o wins");
   });
 
-  it('indicates if there is a win in 2st row', function() {
+  it('indicates if there is a win in 2nd row', function() {
     var gArr = ['o', 'o', 'x', 'x', 'x', 'x', 'o', 'x', 'o'];
     expect(winChecker.rowChecker(gArr)).toEqual("Congratulations! x wins");
   });
 
-  it('indicates if there is a win in 2st row', function() {
+  it('indicates if there is a win in 3rd row', function() {
     var gArr = ['o', 'o', 'x', 'x', 'o', 'x', 'o', 'o', 'o'];
     expect(winChecker.rowChecker(gArr)).toEqual("Congratulations! o wins");
   });
 
+  it('indicates if there is a win in 1st column', function() {
+    var gArr = ['o', 'o', 'x', 'o', 'o', 'x', 'o', 'x', 'o'];
+    expect(winChecker.columnChecker(gArr)).toEqual("Congratulations! o wins");
+  });
+
+  it('indicates if there is a win in 2nd column', function() {
+    var gArr = ['o', 'o', 'x', 'x', 'o', 'x', 'x', 'o', 'o'];
+    expect(winChecker.columnChecker(gArr)).toEqual("Congratulations! o wins");
+  });
+
+  it('indicates if there is a win in 3rd column', function() {
+    var gArr = ['o', 'x', 'o', 'x', 'o', 'o', 'o', 'x', 'o'];
+    expect(winChecker.columnChecker(gArr)).toEqual("Congratulations! o wins");
+  });
 });
--- src/WinChecker.js
+++ src/WinChecker.js
@@ -7,3 +7,9 @@ WinChecker.prototype.rowChecker = function(gArr) {
   if (gArr[3] === gArr[4] && gArr[4] === gArr[5]) { return "Congratulations! " + gArr[3] + " wins" ;}
   if (gArr[6] === gArr[7] && gArr[7] === gArr[8]) { return "Congratulations! " + gArr[6] + " wins" ;}
 };
+
+WinChecker.prototype.columnChecker = function(gArr) {
+  if (gArr[0]=== gArr[3] && gArr[3] === gArr[6]) { return "Congratulations! " + gArr[0] + " wins" ;}
+  if (gArr[1] === gArr[4] && gArr[4] === gArr[7]) { return "Congratulations! " + gArr[1] + " wins" ;}
+  if (gArr[2] === gArr[5] && gArr[5] === gArr[8]) { return "Congratulations! " + gArr[2] + " wins" ;}
+};
